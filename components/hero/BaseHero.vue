aeac3e04edf31457bb7e7b92411e6f4c743f1d9e
Mahalakshmi <mahait07@gmail.com>
2022-03-31 12:46:33 -0600

    styled button

--- components/hero/BaseHero.vue
+++ components/hero/BaseHero.vue
@@ -3,9 +3,9 @@
   <section :style="{ backgroundImage: `url(${image})`}" class="h-96 bg-auto">
 
   <h1 class="pt-10 pl-5 text-white">THIS IS HERO SECTION</h1>  
-   <p class="pt-5 pl-5  text-white ">Description</p>
+   <p class="pt-5 pl-5 mb-6 text-white ">Description</p>
    
-   <a class ="text-white bg-teal-600 rounded " href="href" :@click= onclick > View Courses</a>
+   <a class ="text-white bg-teal-600 rounded-lg p-2 ml-5" href="assets/images/design.png" :@click= onclick > View Courses</a>
 
   </section>
 </template>
