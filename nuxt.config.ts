97e25dd17e550fcaab66572948c01186933a8b07
Stephan Taylor <stephantaylor21@hotmail.com>
2022-03-31 09:25:55 -0600

    added components folder and files

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,15 @@ import { defineNuxtConfig } from "nuxt3";
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
   css: [],
-  components: ["~/components", "~/components/layouts", "~/components/ui"],
+  components: [
+    "~/components",
+    "~/components/layouts",
+    "~/components/ui",
+    "~components/cards",
+    "~components/header",
+    "~components/hero",
+    "~server/api",
+  ],
   buildModules: ["@nuxtjs/tailwindcss"],
   modules: [],
 });
