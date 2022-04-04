92d7797dfba17b52518feb5a37ecbb85e90968e5
Stephan Taylor <stephantaylor21@hotmail.com>
2022-04-04 11:47:06 -0600

    fixed links

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,20 +1,26 @@
 <template>
-  <div class="mapouter">
-    <div class="gmap_canvas">
-      <iframe
-        width="600"
-        height="500"
-        id="gmap_canvas"
-        src="https://maps.google.com/maps?q=calgary&t=&z=13&ie=UTF8&iwloc=&output=embed"
-        frameborder="0"
-        scrolling="no"
-        marginheight="0"
-        marginwidth="0"
-      ></iframe
-      ><a href="https://www.whatismyip-address.com/divi-discount/"></a><br />
-      ><a href="https://www.embedgooglemap.net">google maps embed html</a>
+  <section>
+    <header>
+      <BaseHeader />
+    </header>
+    <div class="mapouter">
+      <div class="gmap_canvas">
+        <iframe
+          width="600"
+          height="500"
+          id="gmap_canvas"
+          src="https://maps.google.com/maps?q=calgary&t=&z=13&ie=UTF8&iwloc=&output=embed"
+          frameborder="0"
+          scrolling="no"
+          marginheight="0"
+          marginwidth="0"
+        ></iframe
+        ><a href="https://www.whatismyip-address.com/divi-discount/"></a><br />
+        ><a href="https://www.embedgooglemap.net">google maps embed html</a>
+      </div>
     </div>
-  </div>
+  </section>
+  <BaseFooter />
 </template>
 
 <style>
@@ -30,14 +36,14 @@
     height: 500px;
     width: 600px;
   }
-  .navbar{
-  width: 100%;
-  height: 60px;
-  background-color:black;
-  bottom: 0;
-  position: fixed;
-  box-shadow: 0.1px 0.1px 15px 0.1px #273c75;
-  border-radius:10px;
-  display: flex
+  .navbar {
+    width: 100%;
+    height: 60px;
+    background-color: black;
+    bottom: 0;
+    position: fixed;
+    box-shadow: 0.1px 0.1px 15px 0.1px #273c75;
+    border-radius: 10px;
+    display: flex;
   }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,13 @@
 <template>
-  <section>
+  <div>
     <BaseHeader />
     <BaseHero />
+
     <section
       class="flex flex-wrap flex-shrink-0 justify-center text-center justify-items-center gap-20 bg-cover mt-16 rounded-md mb-10 h-96"
     >
       <BaseCard />
     </section>
-  </section>
-  <BaseFooter />
+    <BaseFooter />
+  </div>
 </template>
