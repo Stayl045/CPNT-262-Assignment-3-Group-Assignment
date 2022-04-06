00ea52e025f161bec042c20a0830ac4c7bda797f
Mahalakshmi <mahait07@gmail.com>
2022-04-06 13:14:15 -0600

    conflict fixed

--- components/CoursesCard/CouresesDescriptionCard.vue
+++ components/CoursesCard/CouresesDescriptionCard.vue
@@ -1,9 +1,5 @@
 <template>
-<<<<<<< HEAD
-  <p class="pb-5 text-white font-semibold">
-=======
   <p class="text-white pb-3 font-semibold grid p-5 text-center">
->>>>>>> b269f4aaa2cdcb97ab84fa69063558f1ab6124c0
     <slot></slot>
   </p>
 </template>
--- components/CoursesCard/CouresesTitleCard.vue
+++ components/CoursesCard/CouresesTitleCard.vue
@@ -1,11 +1,7 @@
 <template>
-<<<<<<< HEAD
-  <h3 class="h-80 w-auto text-white font-extrabold text-3xl">
-=======
   <h3
     class="grid text-center px-1.5 mt-5 text-white text-3xl font-extrabold sm:grid sm:text-center sm:content-center"
   >
->>>>>>> b269f4aaa2cdcb97ab84fa69063558f1ab6124c0
     <slot> </slot>
   </h3>
 </template>
